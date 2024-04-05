5b9a3722b763fbbe3184a66e45c0040784c199a2
Brian <briangonzaz305@gmail.com>
2024-04-05 20:50:59 -0300

    JSON

--- TP1/fn.js
+++ TP1/fn.js
@@ -1,7 +1,27 @@
-$Array = ['Pera','Manzana','Frutilla','Mandarina','Banana'];
+// Mostrar un elemento del array en particular
+$frutas = ['Pera','Manzana','Frutilla','Mandarina','Banana'];
 
-console.log('Primero ' + $Array[1])
+console.log($frutas[1])
 
-for (i=0; i < $Array.length; i++) {
-    console.log($Array[i])
+console.log(' ')
+
+//recorrer con un for el array
+
+for (i=0; i < $frutas.length; i++) {
+    console.log($frutas[i])
+}
+
+console.log(' ')
+
+// fruta es un elemento (nombre de variable a eleccion)
+$frutas.forEach(fruta => {
+    console.log(fruta)
+});
+
+console.log(' ')
+
+for (const fruta2 of $frutas) {
+    if (fruta2 == 'Manzana') {
+        console.log(fruta2)
+    }
 }
