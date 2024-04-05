beeaf43c6539034fd55edf58d54305b26504501e
Brian <briangonzaz305@gmail.com>
2024-04-04 21:19:02 -0300

    agregue funciones

--- JS/Funciones.js
+++ JS/Funciones.js
@@ -1,1 +1,19 @@
-alert("hola mundo")
+
+function saludar(nombre) {
+    alert ("hola " + nombre);
+}
+
+function suma(numerouno,numerodos) {
+    alert ('el resltado es ' + (numerouno+numerodos));
+}
+
+const constante = 10;
+
+function comparacion (numero) {
+    if (constante<numero) {
+        alert ('el numero es mayor');
+    }
+    else {
+        alert ('el numero es menor');
+    }
+}
