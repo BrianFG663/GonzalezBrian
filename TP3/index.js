45a13819be907e2724840971924477a4f89721ef
Brian Gonzalez <briangonzaz305@gmail.com>
2024-04-20 22:21:55 -0300

    Agregue CSS y arrelge bug mapa inicializado

--- TP3/index.js
+++ TP3/index.js
@@ -1,6 +1,6 @@
-function map (latitude,longitude) {
+function map(latitude, longitude) {
 
-    var map = L.map('map').setView([latitude, longitude], 13);
+    var map = L.map('map').setView([latitude, longitude], 2.5);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
@@ -8,33 +8,54 @@ function map (latitude,longitude) {
     }).addTo(map);
 
     var marker = L.marker([latitude, longitude]).addTo(map);
+
 }
 
-function SearchUser () {
+function SearchUser() {
     const url = 'https://randomuser.me/api/'
-    fetch (url)
-    .then (response => response.json())
-    .then (data => {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
 
-        const latitude = data.results[0].location.coordinates.latitude
-        const longitude = data.results[0].location.coordinates.longitude
-    
-    document.getElementById("Informacion").innerHTML = 
-        `
-        <div class="BoxNames">
-            <p>${data.results[0].name.first} &nbsp;</p>
-            <p>${data.results[0].name.last}</p>
-         </div>
+            const latitude = data.results[0].location.coordinates.latitude
+            const longitude = data.results[0].location.coordinates.longitude
 
-        <div class="BoxLatLon">
-            <p>${data.results[0].location.coordinates.latitude}</p>
-            <p>${data.results[0].location.coordinates.longitude}</p>
-         </div>
+            document.getElementById("ContainMap").classList.add("MapContain")
+            document.getElementById("map").classList.add("Mapstyle")
+            document.getElementById("ContainInfo").classList.add("Informacion")
+            document.getElementById("ContainProfile").classList.add("Profile")
 
-         <div>
-            <img src="${data.results[0].picture.large}">
-        </div>`
+            document.getElementById("ContainProfile").innerHTML =
 
-    map(latitude,longitude)
-    })
+                `<p>Fecha de nacimiento: ${data.results[0].dob.date.slice(0, 10)}</p>
+                 <p>Edad:  ${data.results[0].dob.age} años</p>
+                 <p>Ciudad de naciemiento: ${data.results[0].location.city}</p>
+                 <p>Telefono Celular: ${data.results[0].phone}</p>`
+
+            document.getElementById("ContainInfo").innerHTML =
+                
+                `<div class="BoxNames">
+                    <p>Nombre y apellido: ${data.results[0].name.first} &nbsp;</p>
+                    <p>${data.results[0].name.last}</p>
+                </div>
+
+                <div class="BoxLatLon">
+                    <p>Latitud: (${data.results[0].location.coordinates.latitude}) &nbsp;</p>
+                    <p>Longitud: (${data.results[0].location.coordinates.longitude})</p>
+                </div>
+
+                <div>
+                    <img class="ProfileImage" src="${data.results[0].picture.large}">
+                </div>`
+
+
+            container = L.DomUtil.get('map');
+
+            if (container != null) {
+                container._leaflet_id = null;
+            }
+
+            map(latitude, longitude)
+        })
 }
+
