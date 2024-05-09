ce6887fcac5e0a275dfbc2b0bf1c4d339285a927
Brian Gonzalez <briangonzaz305@gmail.com>
2024-05-09 15:58:17 -0300

    Parcialito dos

--- Practicas/Parcialito 2/JS/Fn.js
+++ Practicas/Parcialito 2/JS/Fn.js
@@ -1,104 +1,137 @@
-ImputUno = []
-ImputDos = []
-Personajes = []
+ImputUno = [];
+ImputDos = [];
+Personajes = [];
  
 function CargarUno() {
+    let auxiliar = Number(document.getElementById("ImputNumberOne").value); 
+    let autenticacion = true;
 
-    if (Number(document.getElementById("ImputNumberOne").value) > 0) {
-
-        if (Number(document.getElementById("ImputNumberOne").value) < 826) {
-
-            ImputUno.push(Number(document.getElementById("ImputNumberOne").value))
-            document.getElementById("ImputNumberOne").value = ""
-            console.log(ImputUno)
-
-                if (ImputUno.length == 3){
-                    document.getElementById('ButtonOne').disabled = true;
-                } 
-                    for (i=0; i<ImputUno; i++){
-                        for (j=0; j<ImputDos.length; j++) {
-                            if (ImputUno[i]=ImputDos[j]){
-                                alert("No se puede ingresar numeros de un mismo ")
-                            }
-                        }
-                    }
-            
-        }
-        else {
-            alert("The Rick and Morty API solo cuenta con 826 personajes disponibles, por favor ingrese un numero mas bajo.")
+    for (i=0; i<ImputDos.length; i++) {    
+        if (auxiliar == ImputDos[i]){
+            alert("Valor ya ingresado intente con otro");
+            autenticacion = false;
         }
     }
-    else {
-        alert("No se puede ingresar un numero menor a cero.")
+
+    for (i=0; i<ImputUno.length; i++){
+        if(auxiliar == ImputUno[i]){
+            alert("Valor ya ingresado intente con otro");
+            autenticacion = false;
+        } 
+    }
+
+    if (autenticacion == true) {
+        if (Number(document.getElementById("ImputNumberOne").value) > 0) {
+            if (Number(document.getElementById("ImputNumberOne").value) < 826) {
+
+                ImputUno.push(Number(document.getElementById("ImputNumberOne").value));
+                document.getElementById("ImputNumberOne").value = "";
+                console.log(ImputUno);
+
+                    if (ImputUno.length == 3){
+                        document.getElementById('ButtonOne').disabled = true;
+                    }
+
+            }
+            else {
+                alert("The Rick and Morty API solo cuenta con 826 personajes disponibles, por favor ingrese un numero mas bajo.");
+            }
+        }
+        else {
+            alert("No se puede ingresar un numero menor o igual a cero.");
+        }
     }
 }
 
 function CargarDos() {
+    let auxiliar = Number(document.getElementById("ImputNumberTwo").value);
+    let autenticacion = true;
 
-    if (Number(document.getElementById("ImputNumberTwo").value) > 0) {
+    for (i=0; i<ImputUno.length; i++) {    
+        if (auxiliar == ImputUno[i]){
+            alert("Valor ya ingresado intente con otro");
+            autenticacion = false;
+        }
+    }
 
-        if (Number(document.getElementById("ImputNumberTwo").value) < 826) {
+    for (i=0; i<ImputDos.length; i++){
+        if(auxiliar == ImputDos[i]){
+            alert("Valor ya ingresado intente con otro");
+            autenticacion = false;
+        } 
+    }
 
-            ImputDos.push(Number(document.getElementById("ImputNumberTwo").value))
-            document.getElementById("ImputNumberTwo").value = ""
-            console.log(ImputDos)
+    if (autenticacion == true) {
+        if (Number(document.getElementById("ImputNumberTwo").value) > 0) {
+
+            if (Number(document.getElementById("ImputNumberTwo").value) < 826) {
+
+                ImputDos.push(Number(document.getElementById("ImputNumberTwo").value));
+                document.getElementById("ImputNumberTwo").value = "";
+                console.log(ImputDos);
 
                 if (ImputDos.length == 3){
                     document.getElementById('ButtonTwo').disabled = true;
                 }
 
-
-                    for (i=0; i<ImputUno; i++){
-                        for (j=0; j<ImputDos.length; j++) {
-                            if (ImputUno[i]=ImputDos[j]){
-                                alert("No se puede ingresar numeros de un mismo ")
-                            }
-                        }
-                    }
+            }
+            else {
+                alert("The Rick and Morty API solo cuenta con 826 personajes disponibles, por favor ingrese un numero mas bajo.");
+            }
         }
         else {
-            alert("The Rick and Morty API solo cuenta con 826 personajes disponibles, por favor ingrese un numero menor")
+            alert("No se puede ingresar un numero menor a cero.");
         }
     }
-    else {
-        alert("No se puede ingresar un numero menor a cero.")
-    }
+}   
 
-}
 
 function BuscarPersonaje() {
-    let Personajes = ImputUno.concat(ImputDos)
-    let medioarray = Personajes.length/2
+    
+    let Personajes = ImputUno.concat(ImputDos);
+    let contadorbotonbuscar = 0;
+    
 
-    fetch ("https://rickandmortyapi.com/api/character/"+Personajes)
-        .then(Response => Response.json())
-        .then(data =>{
-            for (i=0; i<data.length; i++){
-                for (j=0; j<medioarray; j++) {
-                    if(data[i].id == Personajes[j]){
-                        document.getElementById("ImagenesUno").innerHTML += 
-                        `<img src="${data[i].image}" alt="">`
-                }
-            }
+    if (Personajes.length != 6) {
+        alert("Debe ingresar 6 numeros");
     }
+    else{
+        contadorbotonbuscar++;
 
-            for (i=0; i<data.length; i++){
-                for (j=medioarray; j<Personajes.length; j++){
-                    if(data[i].id == Personajes[j]){
-                        document.getElementById("ImagenesDos").innerHTML += 
-                        `<img src="${data[i].image}" alt="">`
+        fetch ("https://rickandmortyapi.com/api/character/"+Personajes)
+            .then(Response => Response.json())
+            .then(data =>{
+                for (i=0; i<data.length; i++){
+                    for (j=0; j<ImputUno.length; j++) {
+                        if(data[i].id == ImputUno[j]){
+                            document.getElementById("ImagenesUno").innerHTML += 
+                            `<img src="${data[i].image}" alt="">`
+                        }
                     }
                 }
+
+                for (i=0; i<data.length; i++){
+                    for (j=0; j<ImputDos.length; j++){
+                        if(data[i].id == ImputDos[j]){
+                            document.getElementById("ImagenesDos").innerHTML += 
+                            `<img src="${data[i].image}" alt="">`
+                        }
+                    }
+                }
+            })
+
+            .catch(error => {
+                document.getElementById("error").innerHTML =
+                    `<div class= "ErrorCatch">
+                        <img src="/Practicas/Parcialito/CSS/Imagenes/error-icon-4.png" class="ImageError">
+                        <p>${error}</p>
+                    </div>`
+            })
+
+
+
+            if (contadorbotonbuscar != 0){
+                document.getElementById('Buscar').disabled = true;
             }
-        })
+    }
 }
-
-
-
-/* problemas a resolver:
-    -no se puede agregar dos valores iguales /// usar funcion true false
-    -bloquear boton una vez se hayan mostrado las imagenes /// contador
-    -internet // ya esta
-    -bloquear los imput
-    -tener que cargar si o si 3 personajes en cada array //// if
-*/
