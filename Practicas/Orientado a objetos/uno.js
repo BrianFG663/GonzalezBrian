78cce037ef8598a1fb6b3a84494fa99b0e545443
Teo Hildt <97321897+teologo-x@users.noreply.github.com>
2024-05-16 21:14:17 -0300

    Orientado a objetos

--- Practicas/Orientado a objetos/uno.js
+++ Practicas/Orientado a objetos/uno.js
@@ -1,3 +1,22 @@
-class Rectangulo{
-    
+class Persona{
+    constructor(nombre,apellido,fecha_nacimiento){ //metodo constructor pasa parametros
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    getEdad() {
+        let anio = new Date()
+        let edad = (anio.getFullYear() - this.fecha_nacimiento);
+
+        return edad
+        // console.log(`${this.nombre} tiene ${edad} años`)
+    }
+
 }
+
+const brian = new Persona ("Brian","Gonzalez",2001) // instanciar personas(objeto)
+
+
+let edad = brian.getEdad()
+console.log(`${brian.nombre} tiene ${edad} años`)
